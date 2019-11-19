846c0cde0b4f5c55be6912a9d1601efc014bd3c0
Iván López <lopezi@objectcomputing.com>
2019-11-19 13:54:52 +0100

    Use Spek2

--- complete/src/test/kotlin/example/micronaut/InfoSpec.kt
+++ complete/src/test/kotlin/example/micronaut/InfoSpec.kt
@@ -5,11 +5,10 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 object InfoSpec: Spek({
     describe("info endpoint") {
